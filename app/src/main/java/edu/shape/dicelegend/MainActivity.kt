c836d76a39197bd123ccaed206d8951a0ca5c9bd
Wally Kong <heihei0307@gmail.com>
2023-05-31 00:22:47 +0800

    - Accelerometer & light sensor initial
    - part of roll dice logic

--- app/src/main/java/edu/shape/dicelegend/MainActivity.kt
+++ app/src/main/java/edu/shape/dicelegend/MainActivity.kt
@@ -1,6 +1,14 @@
 package edu.shape.dicelegend
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,10 +16,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import edu.shape.dicelegend.databinding.ActivityMainBinding
+import java.util.Objects
+import java.util.Random
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+    private var resume = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +44,75 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(lightSensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 10) {
+                if(!resume){
+                    randomDice()
+                    resume = true
+                    Toast.makeText(applicationContext, "Shake Dice Result", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(applicationContext, "Already shake, please turn off the light to reset the game!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    private val lightSensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val lightValue = event.values[0]
+            if(lightValue < 200){
+                Toast.makeText(applicationContext, "Game Reset!", Toast.LENGTH_SHORT).show()
+                var viewNumber = findViewById<TextView>(R.id.dice_number)
+                viewNumber.text = "Shake to random dice"
+                resume = false
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    fun randomDice() {
+        val randomGenerator = Random()
+        val randomNum = randomGenerator.nextInt(6) + 1
+        var viewNumber = findViewById<TextView>(R.id.dice_number)
+        viewNumber.text = "" + randomNum;
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
     }
 }
--- app/src/main/java/edu/shape/dicelegend/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/shape/dicelegend/ui/home/HomeFragment.kt
@@ -29,9 +29,10 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.diceNumber
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        textView.text = "Shake to random dice"
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
