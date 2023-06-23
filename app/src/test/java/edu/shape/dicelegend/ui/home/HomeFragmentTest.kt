2c87b1970ba6b842392d9bf63b452ede11127b74
Wally Kong <heihei0307@gmail.com>
2023-06-24 00:14:43 +0800

    fix unit test issue

--- app/src/test/java/edu/shape/dicelegend/ui/home/HomeFragmentTest.kt
+++ app/src/test/java/edu/shape/dicelegend/ui/home/HomeFragmentTest.kt
@@ -1,17 +1,18 @@
 package edu.shape.dicelegend.ui.home
 
+import edu.shape.dicelegend.utils.DiceUtil
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class HomeFragmentTest{
-    private val testExtra: HomeFragment.Extra = HomeFragment.Extra()
+    private val testExtra: DiceUtil.Companion = DiceUtil
 
     @Test
     fun testPlayerWin(){
         val playerDice = 2
         val aiDice = 1
         val expected = 1
-        assertEquals(expected, testExtra.compareDiceSize(playerDice, aiDice))
+        assertEquals(expected, testExtra.compareNumber(playerDice, aiDice))
     }
 
     @Test
@@ -19,7 +20,7 @@ internal class HomeFragmentTest{
         val playerDice = 3
         val aiDice = 5
         val expected = 2
-        assertEquals(expected, testExtra.compareDiceSize(playerDice, aiDice))
+        assertEquals(expected, testExtra.compareNumber(playerDice, aiDice))
     }
 
     @Test
@@ -27,6 +28,6 @@ internal class HomeFragmentTest{
         val playerDice = 2
         val aiDice = 2
         val expected = 0
-        assertEquals(expected, testExtra.compareDiceSize(playerDice, aiDice))
+        assertEquals(expected, testExtra.compareNumber(playerDice, aiDice))
     }
 }
